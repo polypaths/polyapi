5faabbc6e27fbbb44f5a3e30e22f8b9c9459e3eb
Cliff Weng <weng.cliff@gmail.com>
2019-12-05 10:58:54 -0500

    Initial check-in.

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# polyapi
-a NodeJS wrapper of Polypaths' Web Service API. This is to demonstrate what you can do with Polypaths' web service API.
+# polyapi.exe - A Command Line Tool for Enterprise
+
+polyapi.exe is a standalone command line tool on Windows (or Linux) that allows users to interact with Polypaths Enterprise through their scripts. 
+
+polyapi.exe is a wrapper of Polypaths' Web Service API. You can run a report that output a csv to your local drive, you can also create or update a job from a xml file in your local drive. And of course you can launch a job, query various status of the system, and manage the grid (restart the farm, stop/start any calc or calc central) all from your script that runs anywhere that has http access to yout internal Polypaths Enterprise server.
+
+## Getting Started
+```
+git clone https://github.com/polypaths/polyapi.git
+cd polyapi
+npm update
+npm install -g pkg
+```
+
+Create a api.bat file in this directory
+```
+@echo off
+set PP_API_HOST=cc_server
+set PP_API_USER=admin_user
+set PP_API_PASSWORD=admin_pass
+
+rem === following two lines toggle between launching a built exe versus the script  
+rem polyapi.exe %*
+node index.js %*
+
+echo (ERRORLEVEL=%ERRORLEVEL%)
+```
+
+Run the following command. If you see below message, you are in business! If not, modify the three PP_ environment variables, and/or check your firewall setting based on the error message you get. 
+```
+C:\dev\polyapi>api echo hello world
+hello world
+(ERRORLEVEL=0)
+```
+
+## Usage
+
+Before you can call polyapi.exe, there are three environment variables you need to set:
+
+    set PP_API_HOST=entcalc01
+    set PP_API_USER=adminuser
+    set PP_API_PASSWORD=adminpass
+
+You can set them in your script before your calls, you can use certutil or base64 to encrypt your clear text password. You can also provide them when you provision your host, so no password will be shown in any clear text. 
+
+Once set up, you can issue the following command to test your setup.
+
+    polyapi.exe echo hello world
+
+
+## Syntax
+
+### polyapi.exe <commands>
+
+This syntax allows you to perform various tasks (such as run jobs, cancel jobs and check the status of jobs, Calc Central status, etc…) in Enterprise. For all the commands you can run, please refer to "EntDoc 8. APIs.pdf".
+
+Samples:
+
+- polyapi.exe **run** MR Upload // to run a job named "MR Upload"
+- polyapi.exe **execute** OAS calc **on** MBS Rollup **asof** 20180601 // to run the OAS calc job on account "MBS Rollup" as of 2018-06-01
+- polyapi.exe **status CC**
+
+### polyapi.exe <report_file> report <report options>
+
+the following command will generate a csv report called "report_out.csv"
+
+- polyapi.exe report_out.csv **report** pt Hist Price **on** Market Sheet **asof** 20130103 **from** 20121001 **-secid** FNCI 3 **format csv**
+
+### polyapi.exe importbo in_file
+
+The **importbo** command allows you to create/update anything in Enterprise programmatically
+
+- polyapi.exe **importbo** **in_file** job.xml
+
+### polyapi.exe execute xxx job_file
+
+When running a job that involves a file, job_file argument can usually be used to upload an override the default file.
+
+- polyapi.exe **execute** MR Upload **job_file** `c:\appport\default.mr`
+
+### polyapi.exe <batch_file>
+
+This utility will support a "batch mode". The idea is to allow polyapi to run multiple, depending commands in a script, whose relationship/dependency is hard to implement in a script. 
+
+As of now, other than Enterprise APIs, it also supports following commands:
+
+**cmd** to run a Windows command
+**check-on**/**check-off** to turn on/off the check (default is off), which will exit the script when any job failed. 
+**comment** output comment
+
+More (conditional) commands such as IF, ELSE, LOOP, DIR, EXIT, etc... will be added in the future release. 
+
+Sample usage:
+
+polyapi.exe mybatch.txt // to run all the commands in the file.
+
+mybatch.txt:
+```
+cmd dir
+check-on
+echo hello world
+comment execute MR File Wait 0 asof 20190103
+execute CSV Position Upload
+execute Overnight Calculation on Top Rollup
+run OAS Report
+run Scenario Report
+```
+
+## Return Codes
+
+Status Codes (exit code) from this command line tool. This should be used to drive the workflow in your script.
+```
+-3 : System Errors, such as wrong host name, network error, etc...
+-2 : (Enterprise Job) Failed, Killed, for backward compatibility
+-1 : Enterprise Errors, such as Cannot find the job name
+ 0 : Successful
+ 1 : (Enterprise Job) Finished
+ 2 : (Enterprise Job) Running, Scheduled, Waiting, Pending
+ n : For status queries that returns a number
+```
+
+Some Sample Commands
+```
+polyapi.exe execute quick_upload_mr job_file C:\polypaths\data2\20180802.mr
+polyapi.exe execute quick_upload_mr job_file 20180802.mr
+polyapi.exe status job CF_PF_Upload
+polyapi.exe status job ss_pf_upload
+polyapi.exe status job Dummy Wait
+polyapi.exe status job rerun_pf_upload
+polyapi.exe status job MRUpload22
+polyapi.exe status job dw daily pf upload
+polyapi.exe status num calc
+```
---
+++ index.js
@@ -0,0 +1,181 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+function file2StringArray(filename) {
+    try {
+        if (! fs.existsSync(filename)) { return null; };
+        var content = fs.readFileSync(filename,"utf-8");
+        return content.split("\r\n"); // Windows format
+    } catch (err) {
+        console.log(String(err));
+        return null;
+    }
+};
+async function upload(host,fn) {
+    try {
+        return await require('request-promise').post({url:host,
+            formData: {file: require('fs').createReadStream(fn)}} );
+    } catch (error) {
+        return {error:error.message};
+    }
+}
+// returns {status, data}
+async function api(host,cmd,fname=null) {
+    try {
+        //let ret = await axios.get('http://'+host+'/poly/com.polypaths.ent.gui.pprequest.polyAPI?'+cmd,  
+        //    {headers: { UserName: process.env.PP_API_USER,Password: process.env.PP_API_PASSWORD }});
+
+        if (fname != null) {
+            var options = {
+                method: 'POST',
+                uri: 'http://'+host+'/poly/com.polypaths.ent.gui.pprequest.polyAPI?'+cmd,
+                formData: {
+                    file: {
+                        value: fs.createReadStream(fname),
+                        options: {filename: fname }
+                    }
+                },
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'UserName': process.env.PP_API_USER,
+                    'Password': process.env.PP_API_PASSWORD,
+                    'PostFile': 'True'
+                }
+            };
+            var rp = require('request-promise');
+            let ret = await rp(options).promise();
+            // console.log("File "+fname+" uploaded.");
+            return {status:200, data:ret};
+        } else {
+            let axiosConfig = {headers: { UserName: process.env.PP_API_USER,Password: process.env.PP_API_PASSWORD }};
+            let form = null;
+            try {
+                let ret = await axios.post('http://'+host+'/poly/com.polypaths.ent.gui.pprequest.polyAPI?'+cmd, form, axiosConfig);
+                //console.log(ret);
+                return {status:ret.status, data:ret.data}; //ret.data.trim()
+            } catch (e) {
+                // console.log(e);
+                if (e.response == undefined)
+                    return {status:-1, data:e.errno};
+                else
+                    return {status:e.response.status, data:e.response.data};
+            }
+        }
+    } catch (error) {
+        return {status:error.status, data:"ERROR"+","+error.message};
+    }
+};
+
+async function batch(host,cmd_array){
+    var check_status = false;
+    for (var i=0; i<cmd_array.length; i++) {
+        var arr = cmd_array[i].split(" ");
+        if (arr[0] === "cmd") {
+            var cmd = arr.slice(1).join(' ');
+            const execSync = require('child_process').execSync;
+            execSync(cmd,{stdio: 'inherit'});
+        } else if (arr[0] === "comment" || arr[0] === "//") {
+        } else if (arr[0] === "check-on") {
+            check_status = true;
+            console.log("Check On (exit if failed)");
+        } else if (arr[0] === "check-off") {
+            check_status = false;
+            console.log("Check Off (continue to run)");
+        } else {
+            //console.log(`Running "${cmd_array[i]}" on "${host}"`);
+            var fname = null;
+            for (var a=0;a<arr.length;a++) {
+                if (arr[a] == "job_file" || arr[a] == "in_file") {
+                    fname = arr[a+1];
+                }
+            }
+            var ret = await api(host,cmd_array[i],fname);
+            console.log(ret.data);
+            if (check_status && ret.status !== 200) {
+                process.exit(ret.status);
+            }
+        }
+    }
+    process.exit(0);
+};
+
+const version = "Polyapi v1.03 10/10/2019";
+async function main() {
+    var args = process.argv.slice(2);
+    if (process.env.PP_API_HOST==undefined || process.env.PP_API_USER==undefined || 
+        process.env.PP_API_PASSWORD==undefined) {
+        console.log(version);
+        console.log("Environment variables PP_API_HOST, PP_API_USER and PP_API_PASSWORD need to be set.");
+    } else if (args.length < 1) {
+        console.log(version);
+        console.log("Usage: polyapi.exe <command|batch file|report file>");
+    } else {
+        var host = process.env.PP_API_HOST ; //args[0];
+        var cmd_array = file2StringArray(args[0]);
+
+        if (args.length > 2 && args[1].toUpperCase() === "REPORT") {
+            var command = args.slice(1).join(' ');
+            var ret = await api(host,command);
+            if (ret.status != 200 || (ret.data != undefined && ret.data.includes("ERROR:"))) {
+                console.log(ret.data.trim());
+                process.exit(-1);
+            } else {
+                try{
+                    var fs = require("fs");
+                    fs.writeFileSync(args[0],ret.data,'utf8');
+                    process.exit(0);
+                } catch (error) {
+                    console.log(error.message);
+                    process.exit(-3);
+                }
+            }
+        } else if (cmd_array !== null) {
+            console.log(`Running "${args[0]}" in batch mode on "${host}"`);
+            await batch(host,cmd_array);
+            console.log("Batch finished.");
+        } else {
+
+            var fname = null;
+            for (var a=0;a<args.length;a++) {
+                if (args[a] == "job_file" || args[a] == "in_file") {
+                    fname = args[a+1];
+                    args[a+1] = path.basename(fname);
+                }
+            }
+            var command = args.join(' '); // args.slice(1).join(' ');
+            var ret = await api(host,command,fname);
+
+            var msg = "",ret_number=0;
+            if (ret.data != undefined) {
+                if (typeof ret.data == "number") {
+                    ret_number = ret.data;
+                    console.log(ret_number);
+                    process.exit(ret_number);
+                } else {
+                    msg = ret.data.trim();
+                    console.log(msg);
+                }
+            }
+            if (msg.includes("ERROR:") || msg.includes("not supported"))
+                    process.exit(-1);
+            if (ret.status == 200) {
+                if (msg.includes("ERROR:") || msg.includes("not supported"))
+                    process.exit(-1);
+                else if (msg.includes("Finished"))
+                    process.exit(1);
+                else if (msg.startsWith("Running") || msg.startsWith("Waiting") 
+                    || msg.startsWith("Scheduled") || msg.startsWith("Pending"))
+                    process.exit(2);
+                else if (msg.includes("Failed") || msg.includes("Killed"))
+                    process.exit(-2);
+                else
+                    process.exit(0); // Successful
+            } else {
+                process.exit(-3); // System Error
+            }
+        }
+    }
+};
+
+main();
